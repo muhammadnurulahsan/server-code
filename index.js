c53de8ba1254afe658ecc72b118ba0c18b89f206
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-05-07 16:59:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+//require('crypto').randomBytes(64).toString('hex')
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ msg: "Token is not valid" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// curd-app
+// dflon09ykELly3Zu
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.tchpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
+    console.log("Connected to MongoDB");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    //service delete
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //Order Collection API
+    app.post("/orders", async (req, res) => {
+      const newOrder = req.body;
+      const result = await orderCollection.insertOne(newOrder);
+      res.send(result);
+    });
+
+    // Auth API
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15d",
+      });
+      res.send({ accessToken });
+    });
+
+    // GET ALL ORDERS
+    app.get("/orders", verifyToken, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send("You are not authorized to view this page");
+      }
+    });
+
+
+    //update a service by id
+    app.put("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedService = req.body;
+      const filter = { _id: ObjectId(id) };
+      const option = { upset: true };
+      const updatedDoc = {
+        $set: {
+          name: updatedService.name,
+          price: updatedService.price,
+          description: updatedService.description,
+          img: updatedService.img,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc, option);
+      console.log("updating service", updatedService);
+      res.send(result);
+    });
+  } finally {
+    // client.close();
+  }
+}
+
+
+
+run().catch(console.dir);
+
+//Root Routes
+app.get("/", (req, res) => {
+  res.send("Backend Server IS Running ");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
